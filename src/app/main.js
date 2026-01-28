4d673b27a800b0bc0caf41f3f845e9b6e5b5dd2f
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 16:43:05 +0000

    Add theme initialization during app startup

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/app/app.css";
+import { initTheme } from "@/app/theme/themeMode";
 
+// Initial Theme Load
+initTheme();
 
+// Create and mount Vue app
 createApp(App).mount("#app");
