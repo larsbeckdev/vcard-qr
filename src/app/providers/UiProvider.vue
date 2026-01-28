4222ba5d2262e3ec43bb54ceea833f42389909fe
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 18:19:58 +0000

    Add TypeScript support and theme overrides to UiProvider component

--- src/app/providers/UiProvider.vue
+++ src/app/providers/UiProvider.vue
@@ -1,15 +1,22 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from "vue";
 import { NConfigProvider, darkTheme } from "naive-ui";
 import { useThemeStore } from "@/app/theme/theme.store";
+import { createNaiveThemeOverrides } from "@/app/theme/naive/overrides";
 
+// store
 const { isDark } = useThemeStore();
 
 const nTheme = computed(() => (isDark.value ? darkTheme : null));
+
+const nOverrides = computed(() => {
+  void isDark.value;
+  return createNaiveThemeOverrides();
+});
 </script>
 
 <template>
-  <n-config-provider :theme="nTheme">
+  <n-config-provider :theme="nTheme" :theme-overrides="nOverrides">
     <slot />
   </n-config-provider>
 </template>
