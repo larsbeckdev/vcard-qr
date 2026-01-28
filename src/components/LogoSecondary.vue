f12b0c490501ae8a855849d8e0f87df7973f6959
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 16:03:44 +0000

    Add new LogoSecondary component

---
+++ src/components/LogoSecondary.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  width: {
+    type: [Number, String],
+    default: 160,
+  },
+});
+</script>
+
+<template>
+  <img
+    src="@/assets/png/vCard-QR.png"
+    alt="vcard-qr logo"
+    :style="{ width: typeof width === 'number' ? `${width}px` : width }"
+    class="select-none" />
+</template>
