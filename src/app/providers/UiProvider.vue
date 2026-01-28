76d12f528e8e16aed7ad4495c3544d4c4efa78c3
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 17:14:04 +0000

    Remove unused UiProvider component

---
+++ src/app/providers/UiProvider.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from "vue";
+import { NConfigProvider, darkTheme } from "naive-ui";
+import { useThemeStore } from "@/shared/theme/theme.store";
+
+const { isDark } = useThemeStore();
+
+const nTheme = computed(() => (isDark.value ? darkTheme : null));
+</script>
+
+<template>
+  <n-config-provider :theme="nTheme">
+    <slot />
+  </n-config-provider>
+</template>
