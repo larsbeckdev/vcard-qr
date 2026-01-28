9ac85f5e38760840b923d267f95bef2d6c703c6d
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 19:21:04 +0000

    Refactor layout structure to use Naive UI components for consistent styling and replace Main component with router-view

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
 import UiProvider from "@/app/providers/UiProvider.vue";
+
 import Header from "@/app/layout/default/Header.vue";
-import Main from "@/app/layout/default/Main.vue";
 import Footer from "@/app/layout/default/Footer.vue";
 
-import { NLayout, NLayoutHeader, NLayoutContent, NScrollbar } from "naive-ui";
+import {
+  NLayout,
+  NLayoutHeader,
+  NLayoutContent,
+  NLayoutFooter,
+  NScrollbar,
+} from "naive-ui";
 </script>
 
 <template>
   <UiProvider>
-    <!-- --------------- HEADER ------------------ -->
-    <Header />
-    <!-- --------------- MAIN ------------------ -->
-    <Main />
-    <!-- --------------- FOOTER ------------------ -->
-    <!-- --------------- FOOTER ------------------ -->
-    <Footer />
-     
+    <n-layout style="min-height: 100vh">
+      <!-- Header -->
+      <n-layout-header bordered style="height: 64px">
+        <Header />
+      </n-layout-header>
+
+      <!-- Content -->
+      <n-layout-content style="height: calc(100vh - 64px - 48px)">
+        <n-scrollbar style="height: 100%">
+          <router-view />
+        </n-scrollbar>
+      </n-layout-content>
+
+      <!-- Footer -->
+      <n-layout-footer bordered style="height: 48px">
+        <Footer />
+      </n-layout-footer>
+    </n-layout>
   </UiProvider>
 </template>
