d48ad0cc34da0d7f2abdbba8154df3ac6a8374a1
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 15:19:52 +0000

    Migrate from vanilla JavaScript setup to Vue.js application structure

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <main class="p-6">
+    <h1>Vite + Vue 🚀</h1>
+    <button @click="count++">Count: {{ count }}</button>
+  </main>
+</template>
