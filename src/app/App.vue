19663e39b9707e0b60e34891c6e8b4da0b873aea
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 18:02:17 +0000

    Refactor layout by extracting Footer component and integrating Header and Footer into App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,26 +5,24 @@ import LogoPrimary from "@/components/logo/LogoPrimary.vue";
 import ThemeToggle from "@/features/theme-toggle/ThemeToggle.vue";
 
 import UiProvider from "@/app/providers/UiProvider.vue";
+
+import Header from "@/app/layout/default/Header.vue";
+import Footer from "@/app/layout/default/Footer.vue";
 </script>
 
 <template>
   <UiProvider>
+    <!-- --------------- HEADER ------------------ -->
+    <Header />
     <!-- --------------- MAIN ------------------ -->
     <main>
       <div class="main-inner">
         <router-view />
       </div>
     </main>
+    <!-- --------------- FOOTER ------------------ -->
+    <Footer />
   </UiProvider>
-  <!-- --------------- HEADER ------------------ -->
-  
-
-  <!-- --------------- FOOTER ------------------ -->
-  <footer class="site-footer">
-    <div class="layout site-footer-inner">
-      <p>&copy; 2026 vCard-QR. All rights reserved.</p>
-    </div>
-  </footer>
 </template>
 
 <style scoped></style>
---
+++ src/app/layout/default/Footer.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <footer class="site-footer">
+    <div class="layout site-footer-inner">
+      <p>&copy; 2026 vCard-QR. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
