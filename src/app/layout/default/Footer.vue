0f451aac34e8ac9be3122923e0636c63b38e0c7b
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-28 19:22:38 +0000

    Simplify footer structure by removing redundant <footer> tag

--- src/app/layout/default/Footer.vue
+++ src/app/layout/default/Footer.vue
@@ -1,11 +1,9 @@
 <script setup></script>
 
 <template>
-  <footer class="site-footer">
-    <div class="layout site-footer-inner">
-      <p>&copy; 2026 vCard-QR. All rights reserved.</p>
-    </div>
-  </footer>
+  <div class="layout site-footer-inner">
+    <p>&copy; 2026 vCard-QR. All rights reserved.</p>
+  </div>
 </template>
 
 <style scoped></style>
