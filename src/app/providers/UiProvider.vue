8e483a3e6fe261078bf1bdee818aaab97f2643c6
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 10:31:29 +0000

    Add placement property to notification and message providers in UiProvider component

--- src/app/providers/UiProvider.vue
+++ src/app/providers/UiProvider.vue
@@ -11,7 +11,7 @@ import {
   lightTheme,
 } from "naive-ui";
 
-import { getNaiveOverrides } from "@/app/theme"; 
+import { getNaiveOverrides } from "@/app/theme";
 import { useTheme } from "@/app/composables/useTheme";
 
 const { isDark } = useTheme();
@@ -24,8 +24,8 @@ const themeOverrides = computed(() => getNaiveOverrides(isDark.value));
   <n-config-provider :theme="theme" :theme-overrides="themeOverrides">
     <n-loading-bar-provider>
       <n-dialog-provider>
-        <n-notification-provider>
-          <n-message-provider>
+        <n-notification-provider placement="bottom">
+          <n-message-provider placement="bottom">
             <n-modal-provider>
               <slot />
             </n-modal-provider>
