2bc098c5a4b8de1a08cf3465f5177903965b77e5
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 09:41:45 +0000

    Add support for theme overrides based on light or dark mode

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -1,5 +1,11 @@
 import { darkTheme, lightTheme, type GlobalTheme } from "naive-ui";
+import { themeOverridesDark } from "@/app/theme/overrides/overrides.dark";
+import { themeOverridesLight } from "@/app/theme/overrides/overrides.light";
 
-export function getTheme(isDark: boolean): GlobalTheme | null {
+export function getNaiveTheme(isDark: boolean): GlobalTheme {
   return isDark ? darkTheme : lightTheme;
+}
+
+export function getNaiveOverrides(isDark: boolean) {
+  return isDark ? themeOverridesDark : themeOverridesLight;
 }
