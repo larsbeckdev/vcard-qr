fe317e3f4293fb7542b3962a30459a51f936f5b0
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 09:45:54 +0000

    Refactor theme exports to use specific named exports for improved clarity and organization

--- src/app/theme/index.ts
+++ src/app/theme/index.ts
@@ -1,2 +1,7 @@
-export * from "./theme";
-export * from "./overrides";
+export { getNaiveTheme, getNaiveOverrides } from "./theme";
+
+export { themeOverridesLight } from "@/app/theme/overrides/overrides.light";
+export { themeOverridesDark } from "@/app/theme/overrides//overrides.dark";
+
+export { tokensLight } from "@/app/theme/tokens//tokens.light";
+export { tokensDark } from "@/app/theme/tokens/tokens.dark";
