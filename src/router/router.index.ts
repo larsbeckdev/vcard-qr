8123e9751947ee45efff19883c70a5cfda47501f
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 07:35:19 +0000

    Refactor router configuration to use a default layout with nested routes

--- src/router/router.index.ts
+++ src/router/router.index.ts
@@ -1,8 +1,21 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createRouter, createMemoryHistory } from "vue-router";
 
+import DefaultLayout from "@/app/layout/_defaultLayout.vue";
 import Home from "@/pages/home/Home.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  {
+    path: "/",
+    component: DefaultLayout,
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: Home,
+      },
+    ],
+  },
+];
 
 export const router = createRouter({
   history: createMemoryHistory(),
