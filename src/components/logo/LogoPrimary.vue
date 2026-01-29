e073ea027f5e49283e89ea92e3b9531b0cc6e2ad
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 07:59:53 +0000

    Update alt text for the logo image in LogoPrimary component

--- src/components/logo/LogoPrimary.vue
+++ src/components/logo/LogoPrimary.vue
@@ -2,6 +2,6 @@
 
 <template>
   <a href="/" class="logo-link">
-    <img src="@/assets/png/logo.png" alt="vcard-qr logo" class="select-none" />
+    <img src="@/assets/png/logo.png" alt="dm-logo logo" class="select-none" />
   </a>
 </template>
