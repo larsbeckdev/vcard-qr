d31de652d7195c5fca76611187f28fb1a6b5d819
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 07:47:08 +0000

    Update Footer.vue to simplify styling and improve text alignment

--- src/app/layout/default/Footer.vue
+++ src/app/layout/default/Footer.vue
@@ -1,18 +1,10 @@
 <script setup></script>
 
 <template>
-  <p class="copyright-text text-sm opacity-70">
+  <p
+    class="copyright-text flex h-full align-center justify-center text-sm opacity-70">
     &copy; 2026 vCard-QR. All rights reserved.
   </p>
 </template>
 
-<style scoped>
-/* .copyright-text {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 0.875rem;
-  opacity: 0.7;
-} */
-</style>
+<style scoped></style>
