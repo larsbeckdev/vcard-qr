3633ce8412a3ba351850381c91e524f0a2137d65
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 07:59:33 +0000

    Wrap logo image in a clickable link and remove dynamic width prop

--- src/components/logo/LogoSecondary.vue
+++ src/components/logo/LogoSecondary.vue
@@ -1,17 +1,10 @@
-
-<script setup>
-defineProps({
-  width: {
-    type: [Number, String],
-    default: 160,
-  },
-});
-</script>
+<script setup></script>
 
 <template>
-  <img
-    src="@/assets/png/vCard-QR.png"
-    alt="vcard-qr logo"
-    :style="{ width: typeof width === 'number' ? `${width}px` : width }"
-    class="select-none" />
+  <a href="/" class="logo-link">
+    <img
+      src="@/assets/png/vCard-QR.png"
+      alt="vcard-qr logo"
+      class="select-none" />
+  </a>
 </template>
