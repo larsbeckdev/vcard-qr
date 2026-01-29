b0c0916c50f15e656b04f2a882937fde60428c98
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 07:45:12 +0000

    Update Footer component to use inline styles for copyright text formatting

--- src/app/layout/default/Footer.vue
+++ src/app/layout/default/Footer.vue
@@ -1,16 +1,19 @@
 <script setup></script>
 
 <template>
-  <p class="copyright-text">&copy; 2026 vCard-QR. All rights reserved.</p>
+  <p
+    class="copyright-text flex h-full align-center justify-center text-sm opacity-70">
+    &copy; 2026 vCard-QR. All rights reserved.
+  </p>
 </template>
 
 <style scoped>
-.copyright-text {
+/* .copyright-text {
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 0.875rem;
   opacity: 0.7;
-}
+} */
 </style>
