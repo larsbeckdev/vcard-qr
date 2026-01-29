d408dfde294e2d5f5aa9dd44743d00641d923b8a
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-29 08:00:28 +0000

    Update logo image with responsive width and height classes

--- src/components/logo/LogoPrimary.vue
+++ src/components/logo/LogoPrimary.vue
@@ -2,6 +2,9 @@
 
 <template>
   <a href="/" class="logo-link">
-    <img src="@/assets/png/logo.png" alt="dm-logo logo" class="select-none" />
+    <img
+      src="@/assets/png/logo.png"
+      alt="dm-logo logo"
+      class="select-none w-92 h-auto" />
   </a>
 </template>
